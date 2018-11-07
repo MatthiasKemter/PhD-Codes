da88360e6e2e0be32b30889f5a928ac82217fd9c
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-07 14:13:47 +0100

    Comments

--- Matlab/CN_Generation.m
+++ Matlab/CN_Generation.m
@@ -1,4 +1,12 @@
 function [Adj] = CN_Generation(Data,varargin)
+%CN_GENERATION generates a complex network based on event synchronization.
+%
+%   Events are those entries of the timeseries (Data) that exceed the 
+%   percentile given as the second input variable (percentileES). The
+%   standard value is 0.95. Edges are placed between stations with an
+%   ES value within the percentile given as the third input variable
+%   (percentileLink). The standard value is 0.95. The function returns a
+%   binarized, symmetric adjacency matrix (Adj).
 
 percentileES=0.95;
 percentileLink=0.95;
@@ -11,16 +19,17 @@ if nargin==3
 end
 
 N=size(Data,1);
-ES=zeros(N,N);
+ES=zeros(N,N);  %event synchronization matrix
 
-tx=1:size(Data,2);
+tx=1:size(Data,2);  %timesteps
 ty=1:size(Data,2);
 
-pool=parpool(3);
+pool=parpool(3);    %parallel processing for higher performance
 tic
 for i=1:N
     parfor j=i+1:N
-        [ES(i,j),~]=eventsynchro(tx,Data(i,:),ty,Data(j,:),0,percentileES); %eventsynchro was modified in lines 46+47 to only include high events
+        [ES(i,j),~]=eventsynchro(tx,Data(i,:),ty,Data(j,:),0,percentileES); 
+        %eventsynchro was modified in lines 46+47 to only include high events
     end
 end
 toc
@@ -30,8 +39,7 @@ Adj=ES; %create Adjecency Matrix
 N=size(Adj,1);
 
 Adj=(Adj+Adj').*(1+diag(-1*ones(1,N))); %make A symmetric and substitute zeros for i=j
-T=prctile(Adj(:),percentileLink*100); %define threshold of synchronization as 95 percentile
+T=prctile(Adj(:),percentileLink*100); %define threshold of synchronization
 Adj=Adj>T; %binarize Adjecency Matrix
-
 
 end
