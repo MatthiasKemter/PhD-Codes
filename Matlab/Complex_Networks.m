d34333c72611bcceb164868519a2f5c0f1f5b6bd
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-07 11:46:40 +0100

    Plot of Avg. Distance added

--- Matlab/Complex_Networks.m
+++ Matlab/Complex_Networks.m
@@ -44,10 +44,10 @@ ty=1:size(ROI,2);
 
 myCluster = parcluster('local');
 myCluster.NumThreads = 2;
-parpool(3)
+parpool(4)
 tic
 for i=1:N
-    parfor j=i:N
+    for j=i+1:N
         [ESynchro(i,j),~]=eventsynchro(tx,ROI(i,:),ty,ROI(j,:),0,0.95); %eventsynchro was modified in lines 46+47 to only include high events
     end
     i
@@ -55,9 +55,10 @@ end
 toc
 
 AESy=ESynchro; %create Adjecency Matrix
+N=size(AESy,1);
 
 AESy=(AESy+AESy').*(1+diag(-1*ones(1,N))); %make A symmetric and substitute zeros for i=j
-T=prctile(AESy(:),95); %define threshold of synchronization as 98 percentile
+T=prctile(AESy(:),95); %define threshold of synchronization as 95 percentile
 AESy=AESy>T; %binarize Adjecency Matrix
 
 %%
@@ -65,30 +66,49 @@ AESy=AESy>T; %binarize Adjecency Matrix
 G=graph(AESy);
 Deg=degree(G);
 
-figure(7)
+figure(1)
 h = plot(G);
 h.XData=cell2mat(c(:,4));
 h.YData=cell2mat(c(:,5));
 h.NodeLabel=c(:,3);
 h.NodeCData=Deg;
 h.MarkerSize=10;
+colorbar
 
 [bin,binsize] = conncomp(G);
 idx = binsize(bin) == max(binsize);
 GC = subgraph(G, idx);
-figure(8)
+figure(2)
 h2=plot(GC);
 h2.XData=cell2mat(c(idx,4));
 h2.YData=cell2mat(c(idx,5));
 h2.NodeLabel=[];%c(idx,3);
 h2.NodeCData=Deg(idx);
 h2.MarkerSize=10;
+%h2.EdgeColor='none';
+title('degree');
+colorbar;
 
-figure(9)
+figure(3)
 h2=plot(GC);
 h2.XData=cell2mat(c(idx,4));
 h2.YData=cell2mat(c(idx,5));
 h2.NodeLabel=[];%c(idx,3);
 Betw=centrality(GC,'betweenness');
-h2.NodeCData=Betw;
+h2.NodeCData=Betw/max(Betw);
 h2.MarkerSize=10;
+%h2.EdgeColor='none';
+title('betweenness');
+colorbar;
+
+Dist=mean(distances(GC));
+figure(4)
+h2=plot(GC);
+h2.XData=cell2mat(c(idx,4));
+h2.YData=cell2mat(c(idx,5));
+h2.NodeLabel=[];%c(idx,3);
+h2.NodeCData=Dist;
+h2.MarkerSize=10;
+%h2.EdgeColor='none';
+title('Avg. Distance');
+colorbar;
