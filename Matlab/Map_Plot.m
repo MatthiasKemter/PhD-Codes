f0fd64e789acae85451ef5b14e21efba550dd062
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-08 13:34:38 +0100

    plots network on specified map; 4 subplots for different time windows

--- Matlab/Map_Plot.m
+++ Matlab/Map_Plot.m
@@ -1,17 +1,57 @@
 h=worldmap([47 55],[5 16]);
 load coastlines
-%plotm(coastlat,coastlon);
-land = shaperead('landareas', 'UseGeoCoords', true);
-geoshow(land, 'FaceColor', [0.5 0.7 0.5])
+
+%%
+GER = shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
+[~,index] = sortrows({GER.NAME}.'); GER = GER(index); clear index
+GER=GER(82);
+
+figure(1)
+j=1;
+
+for i=1:5:size(G,2)
+Deg=degree(G{i});
+s(j)=subplot(2,2,j);
+j=j+1;
+mapshow(GER, 'FaceColor', [0.9 0.9 0.7]);
+hold;
+G{i}.Nodes.Name = c(:,3);
+h = plot(G{i});
+h.XData=cell2mat(c(:,4));
+h.YData=cell2mat(c(:,5));
+h.NodeCData=Deg;
+h.MarkerSize=10;
+colorbar
+title(i)
+hold off;
+end
+
+s(1).Position=[0.0 0.53 0.5 0.43];
+s(2).Position=[0.5 0.53 0.5 0.43];
+s(3).Position=[0.0 0.03 0.5 0.43];
+s(4).Position=[0.5 0.03 0.5 0.43];
+
+%%
+%hold
+%h2=worldmap([47 55],[5 16]);
+
+GER = shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
+[~,index] = sortrows({GER.NAME}.'); GER = GER(index); clear index
+GER=GER(82);
+
+mapshow(GER, 'FaceColor', [0.9 0.9 0.7]);
+
+land = shaperead('landareas')%, 'UseGeoCoords', true);
+mapshow(land, 'FaceColor', [0.9 0.9 0.7]);
+h = get(gca,'Children');
+set(gca,'Children',[h(2) h(1)])
 
 rivers = shaperead('worldrivers', 'UseGeoCoords', true);
-geoshow(rivers, 'Color', 'blue')
+geoshow(rivers, 'Color', 'blue','LineWidth',2)
+hold
 
-h2=plotm(cell2mat(c(:,5)),cell2mat(c(:,4)),'o');
-h2.MarkerSize=7;
-h2.MarkerFaceColor='w';
-h2.MarkerEdgeColor='k';
 
 
 cities = shaperead('worldcities', 'UseGeoCoords', true);
 h3=geoshow(cities, 'Marker', 'o','MarkerSize',7,'MarkerFaceColor','red');
+
