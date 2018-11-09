77b12486c13438575201aaf1fec11ddc1d31db02
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-09 14:49:32 +0100

    Update Matlab/Map_Plot.m

--- Matlab/Map_Plot.m
+++ Matlab/Map_Plot.m
@@ -1,3 +1,6 @@
+for i=1:size(A)
+G{i}=graph(A{i});
+end
 
 GER = shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
 [~,index] = sortrows({GER.NAME}.'); GER = GER(index); clear index
@@ -5,31 +8,34 @@ GER=GER(82);
 
 Rivers = shaperead('C:/Users/kemter/Desktop/gauges/Europe_Hydrography.shp');
 [~,index] = sortrows({Rivers.NAME}.'); Rivers = Rivers(index); clear index
-Rivers=Rivers([46 70 99 149 185 225 226 227]);
+Rivers=Rivers([46 70 78 99 149 168 185 225 226 227 231 269 352 353]);
 
 figure(1)
 j=1;
 
-for i=1:1:size(G,2)-1
+for i=1:2:11%size(G,2)
 Deg=degree(G{i});
 s(j)=subplot(2,3,j);
 j=j+1;
 mapshow(GER, 'FaceColor', [0.9 0.9 0.7]);
 hold;
+mapshow(Rivers,'LineWidth',2);
 G{i}.Nodes.Name = c(:,3);
 h = plot(G{i});
 h.XData=cell2mat(c(:,4));
 h.YData=cell2mat(c(:,5));
 h.NodeCData=Deg;
+%Betw=centrality(G{i},'betweenness');
+%h.NodeCData=Betw/max(Betw);
+h.NodeLabel=[];
 h.MarkerSize=8;
 h.LineWidth=0.2;
 h.EdgeColor='k';
-mapshow(Rivers,'LineWidth',2);
 %colorbar
 xlim([5 15]);
 ylim([47 55]);
-firstYear=datestr(c{1,7}(10+(i-1)*offset),'yyyy');
-lastYear=datestr(c{1,7}(10+windowSize+(i-1)*offset),'yyyy');
+firstYear=datestr(c{1,7}(1+(i-1)*offset),'yyyy');
+lastYear=datestr(c{1,7}(1+windowSize+(i-1)*offset),'yyyy');
 title([firstYear '-' lastYear],'FontSize',14);
 hold off;
 axis off;
