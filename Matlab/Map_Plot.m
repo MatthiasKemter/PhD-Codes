84e21843ce3e1e8052c0db49c74a7bbcb96f5bee
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-09 11:17:55 +0100

    added rivers, tweaked plot design, added option for 2x3 subplot

--- Matlab/Map_Plot.m
+++ Matlab/Map_Plot.m
@@ -1,17 +1,18 @@
-h=worldmap([47 55],[5 16]);
-load coastlines
 
-%%
 GER = shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
 [~,index] = sortrows({GER.NAME}.'); GER = GER(index); clear index
 GER=GER(82);
 
+Rivers = shaperead('C:/Users/kemter/Desktop/gauges/Europe_Hydrography.shp');
+[~,index] = sortrows({Rivers.NAME}.'); Rivers = Rivers(index); clear index
+Rivers=Rivers([46 70 99 149 185 225 226 227]);
+
 figure(1)
 j=1;
 
-for i=1:5:size(G,2)
+for i=1:1:size(G,2)-1
 Deg=degree(G{i});
-s(j)=subplot(2,2,j);
+s(j)=subplot(2,3,j);
 j=j+1;
 mapshow(GER, 'FaceColor', [0.9 0.9 0.7]);
 hold;
@@ -20,40 +21,31 @@ h = plot(G{i});
 h.XData=cell2mat(c(:,4));
 h.YData=cell2mat(c(:,5));
 h.NodeCData=Deg;
-h.MarkerSize=10;
-colorbar
+h.MarkerSize=8;
+h.LineWidth=0.2;
+h.EdgeColor='k';
+mapshow(Rivers,'LineWidth',2);
+%colorbar
+xlim([5 15]);
+ylim([47 55]);
 firstYear=datestr(c{1,7}(10+(i-1)*offset),'yyyy');
 lastYear=datestr(c{1,7}(10+windowSize+(i-1)*offset),'yyyy');
-title([firstYear '-' lastYear])
+title([firstYear '-' lastYear],'FontSize',14);
 hold off;
+axis off;
 end
 
-s(1).Position=[0.0 0.53 0.5 0.43];
-s(2).Position=[0.5 0.53 0.5 0.43];
-s(3).Position=[0.0 0.03 0.5 0.43];
-s(4).Position=[0.5 0.03 0.5 0.43];
+s(1).Position=[-0.02 0.53 0.3 0.43];
+s(2).Position=[0.33 0.53 0.3 0.43];
+s(3).Position=[0.67 0.53 0.3 0.43];
+s(4).Position=[-0.02 0.03 0.3 0.43];
+s(5).Position=[0.33 0.03 0.3 0.43];
+s(6).Position=[0.67 0.03 0.3 0.43];
 
-%%
-%hold
-%h2=worldmap([47 55],[5 16]);
+%s(1).Position=[0.0 0.53 0.5 0.43];
+%s(2).Position=[0.5 0.53 0.5 0.43];
+%s(3).Position=[0.0 0.03 0.5 0.43];
+%s(4).Position=[0.5 0.03 0.5 0.43];
 
-GER = shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
-[~,index] = sortrows({GER.NAME}.'); GER = GER(index); clear index
-GER=GER(82);
-
-mapshow(GER, 'FaceColor', [0.9 0.9 0.7]);
-
-land = shaperead('landareas')%, 'UseGeoCoords', true);
-mapshow(land, 'FaceColor', [0.9 0.9 0.7]);
-h = get(gca,'Children');
-set(gca,'Children',[h(2) h(1)])
-
-rivers = shaperead('worldrivers', 'UseGeoCoords', true);
-geoshow(rivers, 'Color', 'blue','LineWidth',2)
-hold
-
-
-
-cities = shaperead('worldcities', 'UseGeoCoords', true);
-h3=geoshow(cities, 'Marker', 'o','MarkerSize',7,'MarkerFaceColor','red');
+%sgtitle('Degree');
 
