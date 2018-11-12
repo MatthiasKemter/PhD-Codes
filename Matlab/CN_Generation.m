128405cfac0b671a42fec2ff1844c8965989dea7
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-12 11:16:26 +0100

    Update Matlab/CN_Generation.m

--- Matlab/CN_Generation.m
+++ Matlab/CN_Generation.m
@@ -29,7 +29,7 @@ tic
 for i=1:N
     tic
     parfor j=i+1:N
-        [ES(i,j),~]=eventsynchro(tx,Data(i,:),ty,Data(j,:),percentileES,percentileLink); %eventsynchro was modified in lines 46+47 to only include high events
+        [ES(i,j),~]=eventsynchro(tx,Data(i,:),ty,Data(j,:),0,percentileES); %eventsynchro was modified in lines 46+47 to only include high events
     end
     toc
 end
