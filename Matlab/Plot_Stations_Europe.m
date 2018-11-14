39137e7479fd519d96b4d5ed386c94b8ca60a49c
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-14 16:23:55 +0100

    plot stations and other data on a map of europe

---
+++ Matlab/Plot_Stations_Europe.m
@@ -0,0 +1,16 @@
+%%
+
+borders=shaperead('C:/Users/kemter/Desktop/gauges/TM_WORLD_BORDERS-0.3.shp');
+tborders=struct2table(borders);
+europe=borders([tborders{:,12}==150]);
+
+Rivers = shaperead('C:/Users/kemter/Desktop/gauges/Europe_Hydrography.shp');
+[~,index] = sortrows({Rivers.NAME}.'); Rivers = Rivers(index); clear index
+
+mapshow(europe, 'FaceColor', [0.9 0.9 0.7]);
+hold;
+mapshow(Rivers,'LineWidth',1);
+h=scatter(cell2mat(discharge(:,4)),cell2mat(discharge(:,3)),50,'r','filled','MarkerEdgeColor','k');
+xlim([-11 40]);
+ylim([36 72]);
+hold off
