5b68b29ae8f6033a3a635ff697e67d7d7714db42
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-22 16:42:53 +0100

    path cleanup

--- Matlab/Plot_Stations_Europe.m
+++ Matlab/Plot_Stations_Europe.m
@@ -4,7 +4,7 @@ borders=shaperead('C:\Users\kemter\Documents\Data\Shapes\TM_WORLD_BORDERS-0.3.sh
 tborders=struct2table(borders);
 europe=borders([tborders{:,12}==150]);
 
-Rivers = shaperead('C:/Users/kemter/Desktop/gauges/Europe_Hydrography.shp');
+Rivers = shaperead('C:\Users\kemter\Documents\Data\Shapes\Europe_Hydrography.shp');
 [~,index] = sortrows({Rivers.NAME}.'); Rivers = Rivers(index); clear index
 
 mapshow(europe, 'FaceColor', [0.9 0.9 0.7]);
