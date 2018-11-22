15e3369ab7a0cc5411bec9f52da9d9c191c4f283
Matthias Kemter <matthias.kemter@gmx.de>
2018-11-22 16:41:01 +0100

    Create a multilayer network layout file

--- Matlab/edgelist_layout.m
+++ Matlab/edgelist_layout.m
@@ -1,4 +1,6 @@
-nodes=(1:(size(discharge,1)+size(subset_time,1)))';
+%% create a layout file that contains the coordinates of each network node
+
+nodes=(1:(size(discharge,1)+size(subset_time,1)))'; %Node IDs
 Lat=[cell2mat(raw(:,3));subset_time(:,end)];
 Lon=[cell2mat(raw(:,4));subset_time(:,end-1)];
 
